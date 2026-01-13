140e2b36d0b5cc120fc2d5f28b1cc4beb73b88dc
Keura <109036590+KeuraChan@users.noreply.github.com>
2026-01-13 16:38:21 -0300

    Add files via upload

---
+++ dados_locais.js
@@ -0,0 +1,16 @@
+const locais = [
+  "nas florestas próximas",
+  "nas montanhas",
+  "em uma caverna",
+  "nas estradas",
+  "nos túneis",
+  "em ruínas nas montanhas",
+  "no topo de uma colina",
+  "na beira de um rio",
+  "na floresta densa",
+  "no templo antigo",
+  "em um acampamento próximo",
+  "nas cavernas subterrâneas",
+  "no cemitério de espadas",
+  "no campo de batalha antigo",
+];
