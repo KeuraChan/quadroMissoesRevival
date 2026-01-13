345c3b899440681f026682a15cb7c65a3c427140
Keura <109036590+KeuraChan@users.noreply.github.com>
2026-01-13 17:31:15 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -19,25 +19,20 @@ window.onload = () => {
     div.className = "missao";
 
     div.innerHTML = `
-      <h3>${missao.tipo} ${missao.alvo}</h3>
-      <p><strong>Local:</strong> ${missao.local}</p>
-
+      <h3>${missao.tipo} ${missao.alvo} ${missao.local}</h3>
       <p><strong>Dificuldade:</strong> ${missao.dificuldade}/15</p>
-      <p><strong>Prazo:</strong> ${missao.dias} dia(s)</p>
-
-      <hr>
-
       <p><strong>Recompensa</strong></p>
-      <p>${missao.ouro}</p>
-      ${
+      <p>${missao.ouro} ${
         missao.recompensaExtra
           ? `<p>e ${missao.recompensaExtra}</p>`
           : ""
-      }
+      }</p>   
+      <p><strong>Prazo:</strong> ${missao.dias} dia(s)</p>
     `;
 
     container.appendChild(div);
   }
 };
+
 
 
